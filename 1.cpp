ca1831abe7f4cf817f843dc6ca7b79b9ad8a1727
Kunal  Kumar <kumarkunal4052002@gmail.com>
2022-07-13 14:33:39 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num=96;
+    if(num>0){
+        cout<<"the number is positive";
+
+    }
+    else if(num<0){
+        cout<<"the number is negative";
+    }
+    else {
+        cout<<"zero";
+    }
+    return 0;
+}
